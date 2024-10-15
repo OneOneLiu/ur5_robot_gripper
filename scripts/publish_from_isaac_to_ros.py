3444cb8d4722a3b5ea45b26aedc0d6f11379e7f1
severAMNL1 <daohui.liu@mail.utoronto.ca>
2024-10-15 23:09:29 +0000

    migrate tube part code to tube manipulation

--- scripts/publish_from_isaac_to_ros.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env python3
-# https://docs.omniverse.nvidia.com/isaacsim/latest/ros2_tutorials/tutorial_ros2_generic_publisher_subscriber.html#isaac-sim-app-tutorial-ros2-generic-publisher-subscriber
-这个得到的是相对于载入的xform的位置, 是不对的，我要的是相对于世界坐标系的位置
--- scripts/read_pose_extension/docs/CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-# Changelog
-
-## [0.1.0] - 2024-08-30
-
-### Added
-
-- Initial version of read object pose Extension
